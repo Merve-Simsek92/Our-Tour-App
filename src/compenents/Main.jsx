ec90cfa8f73bf612419ae849c71cf900b647eb8a
Merve Şimşek <93996187+Merve-Simsek92@users.noreply.github.com>
2022-03-28 11:31:20 +0300

    Vend

--- src/compenents/Main.jsx
+++ src/compenents/Main.jsx
@@ -1,53 +1,54 @@
-import React,{useState} from 'react'
-import Button from './Button';
+import React, { useState } from "react";
+import Button from "./Button";
 import Data from "./Data";
-import "./Main.css"
-function    Main() {
-  
-  
- const [liste,setListe]=useState(Data);
- function handleClick(id){
-  let  tempListe=liste.filter((item)=>id !== item.id)
-  setListe(tempListe)
-   //filter ve map metodları bir liste geri döndürür.(return eder)
-   //bunlardan geri dönüş alabilmem için bir değişkene eşitlemem gerekir
-   //bu değişkenin type objedir listedir
-  console.log(liste)
-
-
-    }
-    if(liste.length===0){
-      return (
-        <div>Önerimiz kalmamıştır</div>
-      )
-    }
-
+import "./Main.css";
+function Main() {
+  const [liste, setListe] = useState(Data);
+  const [isEmpty, setIsEmpty] = useState(false);
+  const handleRead=()=>{
+    setIsEmpty(!isEmpty)
+  }
+  function handleClick(id) {
+    let tempListe = liste.filter((item) => id !== item.id);
+    setListe(tempListe);
+    //filter ve map metodları bir liste geri döndürür.(return eder)
+    //bunlardan geri dönüş alabilmem için bir değişkene eşitlemem gerekir
+    //bu değişkenin type objedir listedir
+    console.log(liste);
+  }
+  // if(liste.length===0){
+  //   return (
+  //     <div>Önerimiz kalmamıştır</div>
+  //   )
+  // }
+  if (liste.length === 0) {
+    setIsEmpty(true);
+  }
 
   return (
-   <div>{
-    
-      liste.map((item)=>{ 
-      const {id,title,price,description:desc,img} =item;
-      return (
+    <div>
+
+      {liste.length=== 0 ? <div>önerimiz kalmamıştır</div> :
+      liste.map((item) => {
+        const { id, title, price, description: desc, img } = item;
+        return (
           <div className="container" key={id}>
-              <div><img className='image' src={img} alt="" /></div>
-              <div className="middle">
-                  <div className="title">{title}</div>
-                  <div className="price">$ {price}</div>
-              </div>
-              <div className="desc">{desc}</div>
-              <Button className="button" onClickPrp={()=>handleClick(id)}>Not Interested</Button>
+            <div>
+              <img className="image" src={img} alt="" />
+            </div>
+            <div className="middle">
+              <div className="title">{title}</div>
+              <div className="price">$ {price}</div>
+            </div>
+            <div className="desc">{isEmpty ?  desc : `${desc.substring(0,200)}...`} <a href="#" onClick={()=>setIsEmpty(!isEmpty)}>{isEmpty ? "show less": "Read More"}</a></div>
+            <Button className="button" onClickPrp={() => handleClick(id)}>
+              Not Interested
+            </Button>
           </div>
-      )
-
-  })}
-</div>
-
-
-
-
-    
-    )
+        );
+      })}
+    </div>
+  );
 }
 
 export default Main;
